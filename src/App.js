f91d315a75e417c82bc7b30029ac54f4709908b6
Jaskaran Singh <jaskaran1523@gmail.com>
2020-08-12 20:35:14 -0700

    Add SectionHeader

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import './App.css';
+import SectionHeader from './components/common/SectionHeader';
 
 function App() {
   return (
     <div className="App">
+      <SectionHeader />
     </div>
   );
 }
