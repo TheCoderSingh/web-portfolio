5eb09686f5c145a87ab4a3b0a36faffca9ee9f27
Jaskaran Singh <jaskaran1523@gmail.com>
2020-08-14 01:11:30 -0700

    Add page components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
 import React from 'react';
 import './App.css';
-import SectionHeader from './components/common/SectionHeader';
+
+import { Grid } from '@material-ui/core'
+
+import Home from './components/pages/Home'
+import About from './components/pages/About'
+import Skills from './components/pages/Skills'
+import Experience from './components/pages/Experience'
+import Projects from './components/pages/Projects'
+import Publications from './components/pages/Publications'
+import Contact from './components/pages/Contact'
 
 function App() {
   return (
     <div className="App">
-      <SectionHeader />
+      <Grid container>
+        <Home />
+        <About />
+        <Skills />
+        <Experience />
+        <Projects />
+        <Publications />
+        <Contact />
+      </Grid>
     </div>
   );
 }
