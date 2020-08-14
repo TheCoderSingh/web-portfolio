8d6c8d76f464233bec74a14848f24353a04b11ab
Jaskaran Singh <jaskaran1523@gmail.com>
2020-08-14 01:47:33 -0700

    Setup grid and add SectionHeader

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,8 +1,17 @@
 import React from 'react'
+import { Grid } from '@material-ui/core'
+import SectionHeader from '../common/SectionHeader'
 
 const Home = () => {
 	return (
-		
+		<Grid container>
+			<Grid item xs={10}>
+				<SectionHeader sectionTitle="Jaskaran Singh" sectionDescription="Full-Stack Developer" />
+			</Grid>
+			<Grid item xs={2}>
+				{/* <MenuIcon /> */}
+			</Grid >
+		</Grid >
 	)
 }
 
