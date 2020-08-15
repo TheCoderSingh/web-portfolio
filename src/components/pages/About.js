c7d0b23c02d0f80ec22353e3b6198ab94a4793ba
Jaskaran Singh <jaskaran1523@gmail.com>
2020-08-15 00:00:42 -0700

    Add SectionHeader

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -2,12 +2,16 @@ import React from 'react'
 import { Grid } from '@material-ui/core'
 import NavIcon from '../common/NavIcon'
 import NavDrawer from '../common/NavDrawer'
+import SectionHeader from '../common/SectionHeader'
 
 const About = (props) => {
 	return (
 		<section id="about" data-aos="fade-up" data-aos-anchor-placement="bottom-center" ref={props.refProp}>
 			<Grid container>
-				<Grid item xs={12}>
+				<Grid item xs={10}>
+					<SectionHeader sectionTitle="About Me" sectionDescription="Pretty interesting! Eh?" />
+				</Grid>
+				<Grid item xs={2}>
 					<NavIcon open={false} />
 					<NavDrawer />
 				</Grid>
