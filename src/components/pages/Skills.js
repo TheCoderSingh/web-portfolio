2f6287b69e28a3dac8aec5fc270780c9d3b0d661
Jaskaran Singh <jaskaran1523@gmail.com>
2020-08-15 14:15:56 -0700

    Add Section Header

--- src/components/pages/Skills.js
+++ src/components/pages/Skills.js
@@ -1,8 +1,22 @@
 import React from 'react'
+import { Grid } from '@material-ui/core'
+import NavIcon from '../common/NavIcon'
+import NavDrawer from '../common/NavDrawer'
+import SectionHeader from '../common/SectionHeader'
 
 const Skills = () => {
 	return (
-		Skills
+		<section id="skills" data-aos="fade-up" data-aos-anchor-placement="bottom-center">
+			<Grid container id="skills-header" >
+				<Grid item xs={10}>
+					<SectionHeader sectionTitle="Skills" sectionDescription="Awesome! Right?" />
+				</Grid>
+				<Grid item xs={2}>
+					<NavIcon open={false} />
+					<NavDrawer />
+				</Grid>
+			</Grid>
+		</section >
 	)
 }
 
