a7bca6a73644bbe65f7eaff83d0d0611bc28758e
Jaskaran Singh <jaskaran1523@gmail.com>
2020-08-14 20:58:56 -0700

    Add and style scroll text

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Grid, Button } from '@material-ui/core'
+import { Grid, Button, Typography } from '@material-ui/core'
 import SectionHeader from '../common/SectionHeader'
 import NavIcon from '../common/NavIcon'
 
@@ -17,6 +17,11 @@ const Home = () => {
 					<Button variant="outlined" size="large" id="resume-btn">
 						Download My Resume
         			</Button>
+				</Grid>
+				<Grid item xs={12}>
+					<Typography id="scroll-text">
+						Scroll down to see how awesome I am...
+					</Typography>
 				</Grid>
 			</Grid >
 		</section>
