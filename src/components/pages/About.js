4d60b350935d730ca5cc9aa2d4df68c3bffce4f1
Jaskaran Singh <jaskaran1523@gmail.com>
2020-08-14 23:55:17 -0700

    Add drawer

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -1,10 +1,18 @@
 import React from 'react'
+import { Grid } from '@material-ui/core'
+import NavIcon from '../common/NavIcon'
+import NavDrawer from '../common/NavDrawer'
 
 const About = (props) => {
 	return (
-		<div data-aos="fade-up" data-aos-anchor-placement="bottom-center" ref={props.refProp}>
-			ABOUT
-		</div>
+		<section id="about" data-aos="fade-up" data-aos-anchor-placement="bottom-center" ref={props.refProp}>
+			<Grid container>
+				<Grid item xs={12}>
+					<NavIcon open={false} />
+					<NavDrawer />
+				</Grid>
+			</Grid >
+		</section>
 	)
 }
 
