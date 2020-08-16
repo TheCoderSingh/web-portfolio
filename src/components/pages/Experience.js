b7b92bf290a7ab7485b60e7ebba329803d88e489
Jaskaran Singh <jaskaran1523@gmail.com>
2020-08-15 19:47:43 -0700

    Add section header

--- src/components/pages/Experience.js
+++ src/components/pages/Experience.js
@@ -1,8 +1,22 @@
 import React from 'react'
+import { Grid } from '@material-ui/core'
+import NavIcon from '../common/NavIcon'
+import NavDrawer from '../common/NavDrawer'
+import SectionHeader from '../common/SectionHeader'
 
 const Experience = () => {
 	return (
-		Experience
+		<section id="experience">
+			<Grid container>
+				<Grid item xs={10}>
+					<SectionHeader sectionTitle="Experience" sectionDescription="I eat computers for breakfast!" />
+				</Grid>
+				<Grid item xs={2}>
+					<NavIcon open={false} />
+					<NavDrawer />
+				</Grid>
+			</Grid>
+		</section>
 	)
 }
 
