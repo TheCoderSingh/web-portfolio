86186daad4d5d02f367159a7039bb54be5672bab
Jaskaran Singh <jaskaran1523@gmail.com>
2020-08-18 04:18:59 -0700

    Remove extra code to fix warnings

--- src/components/common/NavIcon.js
+++ src/components/common/NavIcon.js
@@ -1,25 +1,10 @@
 import React from 'react'
 import MenuIcon from '@material-ui/icons/Menu'
-import MenuOpenIcon from '@material-ui/icons/MenuOpen'
-// import { Icon } from '@material-ui/core';
 
 const NavIcon = (props) => {
-	let menuOpen = props.open
-
-	if (menuOpen) {
-		return (
-			<div className="menu-icon">
-				<MenuOpenIcon />
-			</div>
-		)
-		menuOpen = false
-	} else {
-		return (
-			<div className="menu-icon">
-				<MenuIcon fontSize="large" style={{ color: 'white' }} />
-			</div>
-		)
-	}
+	return (
+		<MenuIcon fontSize="large" style={{ color: 'white' }} />
+	)
 }
 
-export default NavIcon;
+export default NavIcon
