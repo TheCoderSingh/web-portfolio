94745bff5620270a7692e8ece8cc3fced95861c1
Jaskaran Singh <jaskaran1523@gmail.com>
2020-08-20 01:43:49 -0700

    Fix experience style, add background, fix positioning

--- src/components/ExperienceCard.js
+++ src/components/ExperienceCard.js
@@ -5,40 +5,57 @@ import CardActions from '@material-ui/core/CardActions'
 import CardContent from '@material-ui/core/CardContent'
 import Button from '@material-ui/core/Button'
 import Typography from '@material-ui/core/Typography'
+import { List } from '@material-ui/core'
 
 const useStyles = makeStyles({
 	root: {
 		minWidth: 275,
+		background: "none",
+		boxShadow: "none",
+		borderLeft: "solid 4px #2fa52f",
+		marginTop: "20px",
+		marginBottom: "20px"
 	},
-	title: {
-		fontSize: 16,
+	date: {
+		fontSize: 14,
+		color: "#000"
 	},
 	pos: {
 		marginBottom: 12,
+		color: "#2fa52f",
+		fontWeight: "bold"
 	},
+	title: {
+		color: "#000",
+		fontWeight: "bold",
+		fontSize: "1.4em"
+	},
+	description: {
+		textAlign: "justify"
+	}
 })
 
 const ExperienceCard = (props) => {
-	const classes = useStyles()
+	const classes = useStyles();
 
 	return (
 		<Card className={classes.root}>
 			<CardContent>
-				<Typography variant="h5" component="h2">
-					{props.place}
+				<Typography color="textSecondary" className={classes.date} gutterBottom>
+					{props.date}
 				</Typography>
-				<Typography className={classes.title} color="textSecondary" gutterBottom>
+				<Typography variant="h5" component="h2" className={classes.title}>
 					{props.title}
 				</Typography>
 				<Typography className={classes.pos} color="textSecondary">
-					Duties
-					</Typography>
-				<Typography variant="body2" component="p">
-					Date
-        			</Typography>
+					{props.place}
+				</Typography>
+				<Typography variant="body2" component="p" className={classes.description}>
+					{props.description}
+				</Typography>
 			</CardContent>
 			<CardActions>
-				<Button size="small" className={classes.link}>Learn More</Button>
+				<Button size="small">Learn More</Button>
 			</CardActions>
 		</Card>
 	)
--- src/components/pages/Experience.js
+++ src/components/pages/Experience.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Grid } from '@material-ui/core'
+import { Grid, Divider } from '@material-ui/core'
 import NavDrawer from '../common/NavDrawer'
 import SectionHeader from '../common/SectionHeader'
 import ExperienceCard from '../ExperienceCard'
@@ -8,26 +8,29 @@ const Experience = () => {
 	return (
 		<section id="experience">
 			<Grid container>
-				<Grid item xs={10}>
-					<SectionHeader sectionTitle="Experience" sectionDescription="I eat computers for breakfast!" />
-				</Grid>
-				<Grid item xs={2}>
+				<Grid item xs={12}>
 					<NavDrawer />
+				</Grid>
+				<Grid item xs={12}>
+					<SectionHeader sectionTitle="Experience" sectionDescription="I eat computers for breakfast!" />
 				</Grid>
 			</Grid>
 			<div id="exp-cards">
 				<Grid container direction="column" spacing={2}>
 					<Grid item xs={12}>
-						<ExperienceCard place="Notice App" title="Digital Marketing Intern" titleID="noticeapp" />
+						<ExperienceCard title="Freelancer" place="Netscrew Technologies" date="November 2018 - March 2019" description="Working on my startup and actively looking for new projects. Various technologies such as HTML5, CSS3, Angular, Firebase etc. are being used to develop quality products for clients." />
 					</Grid>
 					<Grid item xs={12}>
-						<ExperienceCard place="Statusbrew" title="Bootcamp Intern" titleID="statusbrew" />
+						<ExperienceCard title="Software Engineer (Development In-Charge)" place="Geekers Technologies" date="July 2018 - October 2018" description="Worked on various technologies including Angular, React Native, Django, Laravel etc." />
 					</Grid>
 					<Grid item xs={12}>
-						<ExperienceCard place="Geekers Technologies" title="Software Engineer (Development In-Charge)" titleID="geekers" />
+						<ExperienceCard title="Bootcamp Intern" place="Statusbrew" date="June 2018" description="Worked on the PHP framework Symfony, Docker, Redis and many other technologies." />
 					</Grid>
 					<Grid item xs={12}>
-						<ExperienceCard title="Freelancer" place="Netscrew Technologies" titleID="netscrew" />
+						<ExperienceCard title="Bootcamp Intern" place="Statusbrew" date="June 2017" description="Worked on Angular 4 and created 'Phonebook' and 'Todo' applications." />
+					</Grid>
+					<Grid item xs={12}>
+						<ExperienceCard title="Digital Marketing Intern" place="Notice App" date="November 2016" description="Main responsibilities included marketing and promotion of the app." />
 					</Grid>
 				</Grid>
 			</div>
