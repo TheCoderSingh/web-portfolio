7154c2d926694028285aff33441d3d9ae7b934ed
Jaskaran Singh <jaskaran1523@gmail.com>
2020-08-20 16:44:30 -0700

    Remove background, change structure, change font

--- src/components/ExperienceCard.js
+++ src/components/ExperienceCard.js
@@ -5,33 +5,38 @@ import CardActions from '@material-ui/core/CardActions'
 import CardContent from '@material-ui/core/CardContent'
 import Button from '@material-ui/core/Button'
 import Typography from '@material-ui/core/Typography'
-import { List } from '@material-ui/core'
 
 const useStyles = makeStyles({
 	root: {
 		minWidth: 275,
 		background: "none",
 		boxShadow: "none",
-		borderLeft: "solid 4px #2fa52f",
 		marginTop: "20px",
-		marginBottom: "20px"
+		marginBottom: "20px",
+		textAlign: "center",
 	},
 	date: {
 		fontSize: 14,
-		color: "#000"
+		color: "#000",
+		marginBottom: 12,
+		fontFamily: "Lato, sans-serif"
 	},
 	pos: {
-		marginBottom: 12,
 		color: "#2fa52f",
-		fontWeight: "bold"
+		fontWeight: "bold",
+		fontFamily: "Lato, sans-serif"
 	},
 	title: {
 		color: "#000",
 		fontWeight: "bold",
-		fontSize: "1.4em"
+		fontSize: "1.4em",
+		fontFamily: "Lato, sans-serif"
 	},
 	description: {
-		textAlign: "justify"
+		fontFamily: "Lato, sans-serif"
+	},
+	button: {
+		justifyContent: "center"
 	}
 })
 
@@ -41,20 +46,20 @@ const ExperienceCard = (props) => {
 	return (
 		<Card className={classes.root}>
 			<CardContent>
-				<Typography color="textSecondary" className={classes.date} gutterBottom>
-					{props.date}
-				</Typography>
 				<Typography variant="h5" component="h2" className={classes.title}>
 					{props.title}
 				</Typography>
 				<Typography className={classes.pos} color="textSecondary">
 					{props.place}
 				</Typography>
+				<Typography color="textSecondary" className={classes.date} gutterBottom>
+					{props.date}
+				</Typography>
 				<Typography variant="body2" component="p" className={classes.description}>
 					{props.description}
 				</Typography>
 			</CardContent>
-			<CardActions>
+			<CardActions className={classes.button}>
 				<Button size="small">Learn More</Button>
 			</CardActions>
 		</Card>
--- src/components/pages/Experience.js
+++ src/components/pages/Experience.js
@@ -20,15 +20,19 @@ const Experience = () => {
 					<Grid item xs={12}>
 						<ExperienceCard title="Freelancer" place="Netscrew Technologies" date="November 2018 - March 2019" description="Working on my startup and actively looking for new projects. Various technologies such as HTML5, CSS3, Angular, Firebase etc. are being used to develop quality products for clients." />
 					</Grid>
+					<Divider />
 					<Grid item xs={12}>
 						<ExperienceCard title="Software Engineer (Development In-Charge)" place="Geekers Technologies" date="July 2018 - October 2018" description="Worked on various technologies including Angular, React Native, Django, Laravel etc." />
 					</Grid>
+					<Divider />
 					<Grid item xs={12}>
 						<ExperienceCard title="Bootcamp Intern" place="Statusbrew" date="June 2018" description="Worked on the PHP framework Symfony, Docker, Redis and many other technologies." />
 					</Grid>
+					<Divider />
 					<Grid item xs={12}>
 						<ExperienceCard title="Bootcamp Intern" place="Statusbrew" date="June 2017" description="Worked on Angular 4 and created 'Phonebook' and 'Todo' applications." />
 					</Grid>
+					<Divider />
 					<Grid item xs={12}>
 						<ExperienceCard title="Digital Marketing Intern" place="Notice App" date="November 2016" description="Main responsibilities included marketing and promotion of the app." />
 					</Grid>
